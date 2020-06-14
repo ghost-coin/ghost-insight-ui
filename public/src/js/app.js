946de4519e984aab5c57b3c668596c1f008a5bad
JoaoCampos89 <jcampos893@gmail.com>
2020-06-14 04:17:17 -0300

    continue ghost rebrand

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var defaultLanguage = localStorage.getItem('insight-language') || 'en';
-var defaultCurrency = localStorage.getItem('insight-currency') || 'PART';
+var defaultCurrency = localStorage.getItem('insight-currency') || 'GHOST';
 
 angular.module('insight',[
   'ngAnimate',
